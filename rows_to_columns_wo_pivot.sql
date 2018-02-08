029a4eba5540dc9fa3017e5ed14cdb8eb79e9614
Renee D <renee@plesba.com>
2018-02-08 08:53:59 -0800

    Update rows_to_columns_wo_pivot.sql

--- rows_to_columns_wo_pivot.sql
+++ rows_to_columns_wo_pivot.sql
@@ -1,5 +1,8 @@
---sample records in rows.png
---for sample out see report.png
+--sample input records in rows.png
+--for sample output see report.png
+--The SQL is useful for converting rows of data into columns.  It works when there is a known set of values that are 
+--changed during a product release.  It can be used to 'pivot' data when the RDBMS does not have pivot built-in.  The example
+--uses a SUM function, but it can also use a MAX or MIN with values other than 0 or 1 like Y or N. 
 select a.tag_type,
     sum(a.inapplicableCt) as inapplicablect,
     sum(a.damagedCt)      as damagedCt,
